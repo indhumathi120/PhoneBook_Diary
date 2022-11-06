65e993c89a3eb3638b792ca4678387cb2b1943f7
indhumathi120 <indhumathi.c.cce@sece.ac.in>
2022-11-06 22:47:38 +0530

    1.Updated delete , retrieve

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -4,44 +4,53 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Operation operation=new Operation();
-        Scanner sc=new Scanner(System.in);
-       File file =new File("abc.txt");
-       try{
-           if(file.createNewFile()){
-               System.out.println("File created");
-           }
-           else {
-               System.out.println("File not created");
-           }
-       }
-       catch (IOException e){
-           e.printStackTrace();
-       }
-       System.out.println("Crud operations:");
+        Operation operation = new Operation();
+        Scanner sc = new Scanner(System.in);
+        File file = new File("abc.txt");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created");
+            } else {
+                System.out.println("File not created");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Crud operations:");
         System.out.println("1.Create");
         System.out.println("2.Retrieve");
         System.out.println("2.Update");
         System.out.println("3.Delete");
 
-        int n=sc.nextInt();
-       switch(n){
 
 
-           case 1:
-               System.out.println("Create");
-               operation.Create();
-               break;
-           case 2:
-               System.out.println("Update");
-               operation.Update();
-               break;
-           case 3:
-               System.out.println("Delete");
-               operation.Delete();
-               break;
-       }
+            int n = sc.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.println("Create");
+                    operation.Create();
+                    System.exit(0);
+                case 2:
+                    System.out.println("Retrieve");
+                    Scanner scanner = new Scanner(new File("E:\\Java Programming\\PhoneBook_Diary\\Demo\\abc.txt"));
+                    String content = "";
+                    while (scanner.hasNext()) {
+                        scanner.nextLine();
+                        content = content + scanner.nextLine();
+                        content += "/n";
+                    }
+                    System.out.print(content);
+                case 3:
+                    System.out.println("Update");
+                    operation.Update();
+                    //System.exit(0);
+                case 4:
+                    System.out.println("Delete");
+                    operation.Delete();
+
+                    //System.exit(0);
+            }
 
 
+        }
     }
-}
--- Demo/src/Operation.java
+++ Demo/src/Operation.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -25,11 +26,11 @@ public class Operation {
     Scanner sc=new Scanner(System.in);
     public String Create() throws IOException {
 
-        FileWriter fileWriter=new FileWriter("abc.txt");
         System.out.println("Enter the Name");
         name=sc.nextLine();
         System.out.println("Enter the number");
         number=sc.nextLine();
+        FileWriter fileWriter=new FileWriter("abc.txt");
         fileWriter.write(name);
         fileWriter.write("-");
         fileWriter.write(number);
@@ -44,10 +45,13 @@ public class Operation {
         return "true";
     }
     public String Update(){
+
         return "true";
 
     }
     public String Delete(){
+        File file=new File("abc.txt");
+        file.delete();
         return "true";
     }
 }
